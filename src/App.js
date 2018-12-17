ba60c3772578c224deb46d5c193e975332abfb5e
taha uygun <tahauygun@gmail.com>
2018-12-16 21:23:49 -0800

    firebase connection continues

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import {database} from './firebase';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
 
+  componentDidMount() {
+    database.ref().on('value', (snapshot) => {
+      this.setState({
+        data:snapshot.val()
+      })
+    });
+  }
 
   render() {
     return (
       <div className="App">
-       <div className="App--header">
-         <h2>Welcome to React and Firebase</h2>
-       </div>
+        <div className="App--header">
+          <h2>Welcome to React and Firebase</h2>
+        </div>
         <pre className="App--data">
-          One day, some data from Firebase will go here.
+          {JSON.stringify(this.state.data, null, 2)}
         </pre>
       </div>
     );
